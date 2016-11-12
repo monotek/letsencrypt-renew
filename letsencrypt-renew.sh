97b28f54451c2b99502180294c3a360c113902e0
André Bauer <abauer@magix.net>
2016-11-12 19:46:05 +0100

    script fixes

--- letsencrypt-renew.sh
+++ letsencrypt-renew.sh
@@ -8,6 +8,11 @@
 
 PATH=/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin
 
+if [ ! -f config.yml ]; then
+    echo "config.yml not found! create it from config.yml.dist before running this script!"
+    exit 1
+fi
+
 #config
 LETSENCRYPT_CMD="$(grep le-cmd < config.yml | sed 's/le-cmd: //')"
 LETSENCRYPT_CERTS="$(grep le-certs < config.yml | sed 's/le-certs: //')"
@@ -21,10 +26,6 @@ WEBSERVER="$(grep le-webserver < config.yml | sed 's/le-webserver: //')"
 WEBSERVER_CERTS_DIR="$(grep le-webserver-certs-dir < config.yml | sed 's/le-webserver-certs-dir: //')"
 RESTART_WEBSERVER="$(grep le-webserver-restart < config.yml | sed 's/le-webserver-restart: //')"
 
-if [ ! -f config.yml ]; then
-    echo "config.yml not found! create it from config.yml.dist before running this script!"
-    exit 1
-fi
 
 # use command line arguments for domains or add them to config.yml
 if [ -n "${1}" ]; then
